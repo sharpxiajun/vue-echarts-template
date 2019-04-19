43737ce248e2e0c6670ba85401c6250f3f6d8f0b
chengsmile <1712137183@qq.com>
2019-04-19 18:47:10 +0800

    cxm code

--- src/demo/views/charts/bars/basicBarChart.vue
+++ src/demo/views/charts/bars/basicBarChart.vue
@@ -4,11 +4,23 @@
       <div slot="header" class="clearfix">
         <h1>基础柱状图</h1>
       </div>
-      <div class="chartBox" ref="myChartDemo">
-        <BarChart></BarChart>
+      <div class="flex-layout chart-editor">
+        <div class="flex-1 mr-20">
+          <JsonEditor :json="jsonData" @jsonChange="onJsonChange"></JsonEditor>
+        </div>
+        <div class="flex-1">
+          <BarChart :legendShow="chartParams.legendShow" :legendOrient="chartParams.legendOrient"
+                    :legendType="chartParams.legendType" :legendLeft="chartParams.legendLeft"
+                    :legendTop="chartParams.legendTop" :legendRight="chartParams.legendRight"
+                    :legendBottom="chartParams.legendBottom" :legendTextColor="chartParams.legendTextColor"
+                    :gridLeft="chartParams.gridLeft" :gridTop="chartParams.gridTop" :gridRight="chartParams.gridRight"
+                    :gridBottom="chartParams.gridBottom"
+                    :rotate="chartParams.rotate" :xAxisValue="chartParams.xAxisValue" :axisType="chartParams.axisType" :axisSplitLineColor="chartParams.axisSplitLineColor" :axisLineColor="chartParams.axisLineColor" :labelLimit="chartParams.labelLimit" :labelLimitWidth="chartParams.labelLimitWidth" :axisLabelColor="chartParams.axisLabelColor" :xSplitLineShow="chartParams.xSplitLineShow" :ySplitLineShow="chartParams.ySplitLineShow" :colors="chartParams.colors" :barStack="chartParams.barStack" :barCategoryGap="chartParams.barCategoryGap" :barBorderRadius="chartParams.barBorderRadius" :labelShow="chartParams.labelShow" :chartData="chartParams.chartData">
+          </BarChart>
+        </div>
       </div>
     </el-card>
-    <el-card class="mb-10">
+    <el-card class="mb-10 params-box">
       <div slot="header" class="clearfix">
         <el-radio-group v-model="chooseRadio" size="medium">
           <el-radio-button label="参数调节" value="1"></el-radio-button>
@@ -16,16 +28,27 @@
         </el-radio-group>
       </div>
       <div class="wrapper" v-if="chooseRadio === '参数调节'">
+        <ParamCard title="网格位置参数">
+          <Grid @gridData="gridData"></Grid>
+        </ParamCard>
         <ParamCard title="坐标轴参数">
-          <Axis></Axis>
+          <Axis @axisData="axisData"></Axis>
         </ParamCard>
         <ParamCard title="图例参数">
-          <Legend @showLegend="showLegend" @typeLegend="typeLegend" @orientLegend="orientLegend" @leftLegend="leftLegend" @rightLegend="rightLegend" @topLegend="topLegend" @bottomLegend="bottomLegend"></Legend>
+          <Legend @legendData="legendData"></Legend>
         </ParamCard>
         <ParamCard title="图形参数">
+          <BarParams @barData="barData"></BarParams>
         </ParamCard>
       </div>
-      <div class="wrapper" v-if="chooseRadio === '代码'"></div>
+      <div class="wrapper" v-if="chooseRadio === '代码'">
+        <div class="mb-10" style="height: 36px;">
+          <el-input placeholder="请输入内容" v-model="importCodeTpl"></el-input>
+        </div>
+        <div class="mb-10" style="height: 160px;">
+          <el-input type="textarea" :rows="6" placeholder="请输入内容" v-model="codeTpl"></el-input>
+        </div>
+      </div>
     </el-card>
   </div>
 </template>
@@ -33,27 +56,33 @@
 <script>
   import ParamCard from 'components/paramCard'
   import Legend from 'components/legend'
+  import Grid from 'components/grid'
   import Axis from 'components/axis'
+  import BarParams from './barParams'
   import BarChart from '@/bar/barChart'
   import vueJsonEditor from 'vue-json-editor'
   import { Sketch } from 'vue-color'
+  import {defaultBar} from 'api/demo/chartDefaultParams'
+  import JsonEditor from 'components/jsonEditor'
 
   export default {
     name: 'basicBarChart',
-    components: { BarChart, 'sketch-picker': Sketch, vueJsonEditor, ParamCard, Legend, Axis },
+    components: { BarChart, 'sketch-picker': Sketch, vueJsonEditor, JsonEditor, ParamCard, Legend, Grid, Axis, BarParams },
     data() {
       return {
         chooseRadio: '参数调节',
-        legendShow: true,
-        legendType: 'plain',
-        legendOrient: 'scroll',
-        legendLeft: 'auto',
-        legendRight: 'auto',
-        legendTop: 'auto',
-        legendBottom: 'auto'
+        ...defaultBar(),
+        importCodeTpl: `import BarChart from 'vue-echarts-template/bar/barChart' `
       }
     },
     computed: {
+      jsonData() {
+        return {chartParams: this.chartParams}
+      },
+      codeTpl() {
+        let propsKeys = Object.keys(this.chartParams).map((_) => `:${_}="chartParams.${_}"`)
+        return propsKeys.length === 0 ? `<BarChart${propsKeys.join(' ')}></BarChart>` : `<BarChart ${propsKeys.join(' ')}></BarChart>`
+      }
     },
     created() {
 
@@ -63,27 +92,54 @@
     },
     watch: {},
     methods: {
-      showLegend(show) {
-        this.legendShow = show
+      onJsonChange(json) {
+        console.log('Mock Json', json)
+        this.chartParams = json.chartParams
       },
-      typeLegend(type) {
-        this.legendType = type
+      legendData(legend) {
+        console.log(legend, '===legend====')
+        this.chartParams.legendShow = legend.show
+        this.chartParams.legendType = legend.type
+        this.chartParams.legendOrient = legend.orient
+        this.chartParams.legendLeft = legend.left
+        this.chartParams.legendRight = legend.right
+        this.chartParams.legendTop = legend.top
+        this.chartParams.legendBottom = legend.bottom
+        this.chartParams.legendTextColor = legend.textColor
       },
-      orientLegend(orient) {
-        this.legendOrient = orient
+      gridData(grid) {
+        console.log(grid, '===grid====')
+        this.chartParams.gridLeft = grid.left
+        this.chartParams.gridRight = grid.right
+        this.chartParams.gridTop = grid.top
+        this.chartParams.gridBottom = grid.bottom
       },
-      leftLegend(left) {
-        this.legendLeft = left
+      axisData(axis) {
+        console.log(axis, '===axis====')
+        this.chartParams.rotate = axis.rotate
+        this.chartParams.xAxisValue = axis.xAxisValue
+        this.chartParams.axisType = axis.axisType
+        this.chartParams.axisSplitLineColor = axis.splitLineColor
+        this.chartParams.axisLineColor = axis.lineColor
+        this.chartParams.labelLimit = axis.labelLimit
+        this.chartParams.labelLimitWidth = axis.labelLimitWidth
+        this.chartParams.axisLabelColor = axis.labelColor
+        this.chartParams.xSplitLineShow = axis.xSplitLineShow
+        this.chartParams.ySplitLineShow = axis.ySplitLineShow
       },
-      rightLegend(right) {
-        this.legendRight = right
-      },
-      topLegend(top) {
-        this.legendTop = top
-      },
-      bottomLegend(bottom) {
-        this.legendBottom = bottom
+      barData(bar) {
+        console.log(bar, bar.colors.split(','), bar.barBorderRadius.split(','), typeof bar.colors.split(','), '===bar====')
+        this.chartParams.colors = bar.colors.split(',')
+        this.chartParams.barStack = bar.barStack
+        this.chartParams.barCategoryGap = bar.barCategoryGap
+        var barBorderRadius = bar.barBorderRadius.split(',')
+        barBorderRadius = barBorderRadius.map(item => {
+          return parseInt(item)
+        })
+        this.chartParams.barBorderRadius = barBorderRadius
+        this.chartParams.labelShow = bar.labelShow
       }
+      
     }
   }
 </script>
